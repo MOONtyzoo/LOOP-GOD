148c5fcb959f5723ab89e5f3dc27d41d6c184ecf
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-07-30 19:44:28 -0700

    Player horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Horizontal Movement")]
+    [SerializeField] private float forwardSpeedMax;
+    [SerializeField] private float backwardSpeedMax;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float turnFactor;
+
+    private Rigidbody2D rbody;
+
     private int trackNum = 2;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
@@ -12,6 +25,7 @@ public class Player : MonoBehaviour
     private void HandleInput()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        MoveHorizontal(horizontalInput);
 
         if (Input.GetButtonDown("MoveUp"))
         {
@@ -37,5 +51,23 @@ public class Player : MonoBehaviour
         {
             Debug.Log("Jump!");
         }
+    }
+
+    private void MoveHorizontal(float inputDirection)
+    {
+        float targetVelocityX = 0;
+        if (inputDirection == 0) {
+            targetVelocityX = 0;
+        } else if (inputDirection > 0) {
+            targetVelocityX = forwardSpeedMax;
+        } else if (inputDirection < 0) {
+            targetVelocityX = -backwardSpeedMax;
+        }
+
+        float maxDelta = acceleration * Time.deltaTime;
+        bool shouldApplyTurnFactor = Mathf.Sign(inputDirection) != Mathf.Sign(rbody.linearVelocityX);
+        if (shouldApplyTurnFactor) maxDelta *= turnFactor;
+
+        rbody.linearVelocityX = Mathf.MoveTowards(rbody.linearVelocityX, targetVelocityX, maxDelta);
     }
 }
