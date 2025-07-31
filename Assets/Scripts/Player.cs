46fce728a37aa8c5b871775549e2f0058666b966
Cristian Melgoza <cpmelgoza05@gmail.com>
2025-07-31 16:43:52 -0700

    Refactor movement code to TrackBody

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,26 +6,8 @@ public class Player : MonoBehaviour
 {
     public event Action OnHit;
 
-    private Rigidbody2D rbody;
+    private TrackBody trackBody;
     private Hitbox hitbox;
-
-    [Header("Horizontal Movement")]
-    [SerializeField] private float forwardSpeedMax;
-    [SerializeField] private float backwardSpeedMax;
-    [SerializeField] private float acceleration;
-    [SerializeField] private float turnFactor;
-
-    [Header("Track Data")]
-    [SerializeField] private float trackDistance;
-    [SerializeField] private int numTracks;
-    [SerializeField] private int trackIdxTop;
-    [SerializeField] private int trackIdxBottom;
-    [SerializeField] private int trackIdxMiddle;
-    private int trackIdx;
-
-    [Header("Track Movement")]
-    [SerializeField] private float trackSwitchDuration;
-    private bool isChangingTracks = false;
 
     [Header("Jump")]
     [SerializeField] private float jumpHoldTime;
@@ -62,14 +44,14 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
-        rbody = GetComponent<Rigidbody2D>();
+        trackBody = GetComponentInChildren<TrackBody>();
         hitbox = GetComponentInChildren<Hitbox>();
         swordHurtbox.Disable();
 
-        trackIdx = trackIdxMiddle;
+        trackBody.SetTrack(2);
 
-        MoveUpButton = new InputButton("MoveUp", 0.5f * trackSwitchDuration);
-        MoveDownButton = new InputButton("MoveDown", 0.5f * trackSwitchDuration);
+        MoveUpButton = new InputButton("MoveUp", 0.5f * trackBody.GetTrackSwitchDuration());
+        MoveDownButton = new InputButton("MoveDown", 0.5f * trackBody.GetTrackSwitchDuration());
         SwordButton = new InputButton("Sword", 0.0f);
         GunButton = new InputButton("Gun", 0.0f);
         JumpButton = new InputButton("Jump", 0.0f);
@@ -85,7 +67,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        MoveHorizontal(horizontalInput);
+        trackBody.MoveHorizontal(horizontalInput);
     }
 
     private void Hitbox_OnHit()
@@ -98,14 +80,14 @@ public class Player : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         UpdateInputButtons();
 
-        if (MoveUpButton.WasPressed() && CanMoveUp())
+        if (MoveUpButton.WasPressed() && trackBody.CanMoveUp())
         {
-            MoveUp();
+            trackBody.MoveUp();
         }
 
-        if (MoveDownButton.WasPressed() && CanMoveDown())
+        if (MoveDownButton.WasPressed() && trackBody.CanMoveDown())
         {
-            MoveDown();
+            trackBody.MoveDown();
         }
 
         if (JumpButton.WasPressed() && CanJump())
@@ -132,65 +114,6 @@ public class Player : MonoBehaviour
         SwordButton.Update();
         GunButton.Update();
         JumpButton.Update();
-    }
-
-    private void MoveHorizontal(float inputDirection)
-    {
-        float targetVelocityX = 0;
-        if (inputDirection == 0)
-        {
-            targetVelocityX = 0;
-        }
-        else if (inputDirection > 0)
-        {
-            targetVelocityX = forwardSpeedMax;
-        }
-        else if (inputDirection < 0)
-        {
-            targetVelocityX = -backwardSpeedMax;
-        }
-
-        float maxDelta = acceleration * Time.deltaTime;
-        bool shouldApplyTurnFactor = inputDirection == 0.0f || Mathf.Sign(inputDirection) != Mathf.Sign(rbody.linearVelocityX);
-        if (shouldApplyTurnFactor) maxDelta *= turnFactor;
-
-        rbody.linearVelocityX = Mathf.MoveTowards(rbody.linearVelocityX, targetVelocityX, maxDelta);
-    }
-
-
-    private bool CanMoveUp() => trackIdx != trackIdxTop && !isChangingTracks;
-    private void MoveUp()
-    {
-        StartCoroutine(changeTrackCoroutine(transform.position.y + trackDistance, trackIdx - 1));
-    }
-
-    private bool CanMoveDown() => trackIdx != trackIdxBottom && !isChangingTracks;
-    private void MoveDown()
-    {
-        StartCoroutine(changeTrackCoroutine(transform.position.y - trackDistance, trackIdx + 1));
-    }
-
-    private IEnumerator changeTrackCoroutine(float targetPosY, int targetTrackIdx)
-    {
-        float duration = trackSwitchDuration;
-        float timer = 0;
-        float timerNormalized;
-
-        float startPosY = transform.position.y;
-
-        isChangingTracks = true;
-        trackIdx = targetTrackIdx;
-        while (timer < duration)
-        {
-            timer += Time.deltaTime;
-            timerNormalized = timer / duration;
-
-            float cubicLerpVal = 1 + Mathf.Pow(timerNormalized - 1, 3f);
-            float lerpedPosY = Mathf.Lerp(startPosY, targetPosY, cubicLerpVal);
-            rbody.position = new Vector2(rbody.position.x, lerpedPosY);
-            yield return new WaitForEndOfFrame();
-        }
-        isChangingTracks = false;
     }
 
     private bool CanJump() => isJumping == false;
---
+++ Assets/Scripts/TrackBody.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TrackBody : MonoBehaviour
+{
+    [Header("Horizontal Movement")]
+    [SerializeField] private float forwardSpeedMax;
+    [SerializeField] private float backwardSpeedMax;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float turnFactor;
+
+    [Header("Track Movement")]
+    [SerializeField] private float trackSwitchDuration;
+
+    private const float TRACK_DISTANCE = 2.0f;
+    private const int TRACK_IDX_TOP = 0;
+    private const int TRACK_IDX_MIDDLE = 2;
+    private const int TRACK_IDX_BOTTOM = 4;
+    private const int NUM_TRACKS = 5;
+
+    private bool isChangingTracks = false;
+    private int trackIdx = 2;
+
+    private Rigidbody2D rbody;
+
+    private void Awake()
+    {
+        rbody = GetComponentInParent<Rigidbody2D>();
+    }
+
+    public bool CanMoveUp() => trackIdx != TRACK_IDX_TOP && !isChangingTracks;
+    public void MoveUp()
+    {
+        StartCoroutine(changeTrackCoroutine(trackIdx - 1));
+    }
+
+    public bool CanMoveDown() => trackIdx != TRACK_IDX_BOTTOM && !isChangingTracks;
+    public void MoveDown()
+    {
+        StartCoroutine(changeTrackCoroutine(trackIdx + 1));
+    }
+
+    private IEnumerator changeTrackCoroutine(int targetTrackIdx)
+    {
+        float duration = trackSwitchDuration;
+        float timer = 0;
+        float timerNormalized;
+
+        float startPosY = transform.position.y;
+        float targetPosY = GetTrackPosY(targetTrackIdx);
+
+        isChangingTracks = true;
+        SetTrack(targetTrackIdx);
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            timerNormalized = timer / duration;
+
+            float cubicLerpVal = 1 + Mathf.Pow(timerNormalized - 1, 3f);
+            float lerpedPosY = Mathf.Lerp(startPosY, targetPosY, cubicLerpVal);
+            rbody.position = new Vector2(rbody.position.x, lerpedPosY);
+            yield return new WaitForEndOfFrame();
+        }
+        isChangingTracks = false;
+    }
+
+    public void MoveHorizontal(float inputDirection)
+    {
+        float targetVelocityX = 0;
+        if (inputDirection == 0)
+        {
+            targetVelocityX = 0;
+        }
+        else if (inputDirection > 0)
+        {
+            targetVelocityX = forwardSpeedMax;
+        }
+        else if (inputDirection < 0)
+        {
+            targetVelocityX = -backwardSpeedMax;
+        }
+
+        float maxDelta = acceleration * Time.deltaTime;
+        bool shouldApplyTurnFactor = inputDirection == 0.0f || Mathf.Sign(inputDirection) != Mathf.Sign(rbody.linearVelocityX);
+        if (shouldApplyTurnFactor) maxDelta *= turnFactor;
+
+        rbody.linearVelocityX = Mathf.MoveTowards(rbody.linearVelocityX, targetVelocityX, maxDelta);
+    }
+
+    public void SetTrack(int newTrackIdx)
+    {
+        trackIdx = Mathf.Clamp(newTrackIdx, TRACK_IDX_TOP, TRACK_IDX_BOTTOM);
+        rbody.position = new Vector2(rbody.position.x, GetTrackPosY(trackIdx));
+    }
+
+    private float GetTrackPosY(int trackIdx)
+    {
+        float trackPosY = TRACK_DISTANCE * -(trackIdx - (NUM_TRACKS - 1) / 2);
+        return trackPosY;
+    }
+
+    public float GetTrackSwitchDuration() => trackSwitchDuration;
+}
